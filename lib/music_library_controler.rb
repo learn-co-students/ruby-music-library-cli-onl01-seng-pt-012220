285a4abd76690333527057ff34abb0c6896ddec7
Charles Pelton <charliejp0311@gmail.com>
2020-02-22 00:54:55 +0000

    Done.

--- lib/music_library_controler.rb
+++ lib/music_library_controler.rb
@@ -7,6 +7,7 @@ class MusicLibraryController
   end
 
   def call
+    user_input = nil
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -16,6 +17,10 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    until user_input == "exit"
+        user_input = gets
+    end
+
   end
 
 end
