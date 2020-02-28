b565f1a34ce78808b924d5b22b5d609d78c2c40e
ohjacksn <jacksonmo@me.com>
2020-02-28 17:53:34 -0500

    Done.

---
+++ lib/artists.rb
@@ -0,0 +1,39 @@
+class Artist
+    extend Concerns::Findable
+
+    attr_accessor :name
+    attr_reader :songs
+    
+    @@all = []
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        self.class.all.push self
+    end
+
+    def self.destroy_all
+        all.clear
+    end
+
+    def self.create(name)
+        artist = new(name)
+        artist.save
+        artist
+    end
+    
+    def add_song(song)
+        song.artist = self unless song.artist
+        songs.push song unless songs.include?(song)
+    end
+
+    def genres
+        songs.collect{|x| x.genre}.uniq
+    end
+end
---
+++ lib/music_importer.rb
@@ -0,0 +1,9 @@
+class MusicImporter
+    attr_reader :path
+
+    def intialize(path)
+        @path = path
+    end
+
+
+end
